1f16c05fcb1afe975ca7944e22919b7a8f3b88b6
ntefera530 <nathan.tefera@outlook.com>
2025-08-02 14:40:04 -0700

    Added PostgreSQL for data storage

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,40 @@
 import express from "express"
 
 import dotenv from "dotenv"
+
+import pool from './DB/db.js';
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
+
+(async () => {
+  try {
+    const res = await pool.query('SELECT current_database()');
+    console.log('Connected! Time:', res.rows[0]);
+
+    const user = await pool.query('SELECT * FROM "Users"');
+    console.log('Users:', user.rows);
+
+  } catch (err) {
+    console.error('DB connection error:', err);
+  } finally {
+    await pool.end();
+  }
+})();
+
+// async function getUsers() {
+//   try {
+//     const result = await pool.query('SELECT * FROM Users');
+//     console.log(result.rows); // [{ id: 1, username: 'alice', ...}, ...]
+//   } catch (err) {
+//     console.error('Database error:', err);
+//   } finally {
+//     await pool.end(); // Close connection when done
+//   }
+// }
+
+// getUsers();
 
 app.listen(PORT, () => {
     console.log("Server is Listening on port: " + PORT)
