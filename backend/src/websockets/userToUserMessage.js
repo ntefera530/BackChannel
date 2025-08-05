a8bd009756e0601202c610dd433f34162e8ad787
ntefera530 <nathan.tefera@outlook.com>
2025-08-05 13:29:31 -0700

    added logic to message Specific WB clients

--- backend/src/websockets/userToUserMessage.js
+++ backend/src/websockets/userToUserMessage.js
@@ -1,6 +1,17 @@
-export const sendMessageToUser = (ws, sender, recipient, data) => {
-    console.log(`User ${sender} --> User "${recipient}": ${data}`);
+import WebSocket from "ws";
+export const sendMessageToUser = (ws, sender, recipient, data, clientsMap) => {
+    //console.log(`User ${sender} --> User "${recipient}": ${data}`);
+   
+    // Need to parse string id to userID, fix this??
+    const client = clientsMap.get(parseInt(recipient));
+    console.log(clientsMap);
 
+    if(client && client.readyState === WebSocket.OPEN){
+        client.send(data);
+    }
+    else{
+        console.log(`${recipient} is not connected`);
+    }
     //If Other user is connected, send to them
 
     //If not then store to be sent later.
--- backend/src/websockets/websocket.js
+++ backend/src/websockets/websocket.js
@@ -5,51 +5,62 @@ import jwt from "jsonwebtoken"
 
 export const setUpWebSocket = (wss) => {
 
-    //Map of clients conected to server (id, websocet object);
-    const clientsMap = new Map();
+  //Map of clients conected to server (id, websocet object);
+  const clientsMap = new Map();
 
-    wss.on('connection', (ws, req) => {
-      console.log('Client connected');
+  wss.on('connection', (ws, req) => {
+    console.log('Client connected');
 
     //Need to get JWT from cookie -- currently hard coded into websocekt header. TODO remove hard coding
     const token = req.headers.cookie;
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-    const clientId = decoded.userId;
-    console.log(decoded);
-    console.log(clientId);
-
-    clientsMap.set(clientId, ws);
-
+    if(!token){
+      ws.close(); 
+      return;
+    }
     
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const clientId = decoded.userId;
+
+    if(clientId){
+      console.log(clientId);
+      //store the userId with their websoccket connection - manage diffrent users
+      clientsMap.set(clientId, ws);
 
       ws.on('message', (message) => {
         console.log(`Received: ${message}`);
         const { type, data, sender, recipient } = JSON.parse(message);
-        console.log(type);
-        console.log(clientsMap.get(clientId).readyState);
 
         switch(type){
             case "sendMessageToUser":
-                sendMessageToUser(ws, sender, recipient, data);
+                sendMessageToUser(ws, sender, recipient, data, clientsMap);
             break;
 
             case "sendMessageToGroup": 
                 sendMessageToGroup(ws, sender, recipient, data);
             break;
         }
+        
         // Echo the message back to all connected clients
-        wss.clients.forEach((client) => {
-          if (client.readyState === WebSocket.OPEN) {
-            client.send(`Server received: ${message}`);
-          }
-        });
+        // wss.clients.forEach((client) => {
+        //   if (client.readyState === WebSocket.OPEN) {
+        //     client.send(`Server received: ${message}`);
+        //   }
+        // });
+
       });
     
       ws.on('close', () => {
         console.log('Client disconnected');
         clientsMap.delete(clientId);
       });
+
+
+    }
+    else{
+      //if no userId  - close the connection
+      ws.close();
+    }
+
     });
 }
 
