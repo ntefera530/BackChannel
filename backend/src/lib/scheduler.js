9561cdbd681bd63b5ecc6fe1c728c4b2efb02354
ntefera530 <nathan.tefera@outlook.com>
2026-08-17 18:15:21 -0700

    Added Errer handling for when Redis hits Free Tier limits

--- backend/src/lib/scheduler.js
+++ backend/src/lib/scheduler.js
@@ -1,15 +1,19 @@
 import { messageDeletionQueue } from "../queues/messageDeletionQueue.js";
 
 export async function startScheduler() {
+    try{
+      await messageDeletionQueue.add(
+      "delete-expired-messages",
+      {},
+      {
+        repeat: { every: 30000 }, // every 30 seconds
+        removeOnComplete: true,
+      }
+    );
 
-  await messageDeletionQueue.add(
-    "delete-expired-messages",
-    {},
-    {
-      repeat: { every: 30000 }, // every 30 seconds
-      removeOnComplete: true,
-    }
-  );
+    console.log("Scheduler started");
+  } catch (error) {
+    console.error("Error starting scheduler, Upstash might have hit limit:", error);
+  }
 
-  console.log("Scheduler started");
 }
