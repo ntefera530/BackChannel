7b7d88acf0265cce1deefdcc0b552ce94ee977fe
ntefera530 <nathan.tefera@outlook.com>
2026-08-17 17:51:08 -0700

    added Toast to Frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import './index.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import {BrowserRouter} from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
 import App from './App.jsx'
 import UserProvider from "./contexts/UserContext.jsx";
 import ChatsProvider from './contexts/ChatContext.jsx';
